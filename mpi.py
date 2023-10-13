f5bec510875fe78106f02e607f135b21416f0c58
AnsgarHe <147828207+AnsgarHe@users.noreply.github.com>
2023-10-13 15:02:10 +0200

    Update mpi.py

--- mpi.py
+++ mpi.py
@@ -2,21 +2,21 @@ import numpy as np
 from mpi4py import MPI
 from LaplaceSolver import LaplaceSolver
 import matplotlib.pyplot as plt
-import seaborn as sns
-import matplotlib.pyplot as plt
+import sys
+
 
 def plot_and_save_heatmap(matrix, filename):
     plt.figure(figsize=(10, 8))
-    plt.imshow(matrix, cmap='hot', interpolation='nearest', vmin = -10, vmax= 40)
+    plt.imshow(matrix, cmap='hot', interpolation='nearest')
     plt.colorbar(label='Value')
     plt.title('Heatmap of ' + filename)
     plt.savefig(filename + '.png')
     plt.close()
 
-# mpiexec /np 4 python3 mpicom.py
+
 "Builds rooms, sets the boundary vals for each boundary,"
 "and assigns the different ranks to solve on the rooms"
-dx = (1/20)
+dx = (1/10)
 
 n = 10
 
@@ -25,7 +25,7 @@ Omega_2 = LaplaceSolver(1,2, dx, {'North': 'Dirichlet', 'East': 'Dirichlet', 'So
 Omega_3 = LaplaceSolver(1,1, dx, {'North': 'Dirichlet', 'East': 'Dirichlet', 'South': 'Dirichlet', 'West': 'Neumann'} )
 # Omega_4 = LaplaceSolver(0.5, 0.5, dx, {'North': 'Dirichlet', 'East': 'Dirichlet', 'South': 'Dirichlet', 'West': 'Neumann'})
    
-print('here')
+#print('here')
 # Specify specific bound vals.
 Gamma_heater = 40*np.ones(Omega_1.N)
 Gamma_window = 5*np.ones(Omega_1.N)
@@ -48,11 +48,6 @@ U_1 = Omega_1.U
 U_2 = Omega_2.U
 U_3 = Omega_3.U
 
-# Start with 15 everywhere
-U_1 = 15*np.ones((Omega_1.N, Omega_1.M))
-U_2 = 15*np.ones((Omega_2.N, Omega_2.M))
-U_3 = 15*np.ones((Omega_3.N, Omega_3.M))
-
 omega = 0.8
 
 ## Måste köra mpirun --oversubscribe -np 3 python3 mpicom.py
@@ -71,10 +66,11 @@ for i in range(n):
             bound_1 = comm.recv(source = 1, tag = 1)
             bound_2 = comm.recv(source = 3, tag = 4)
             hr = int(Omega_2.N/2)
+            
             # Relax boundary conditions as well
             dc_west = np.append(U_2[0:hr,0], bound_1)
             dc_west = omega*dc_west + (1-omega)*Omega_2.get_Dirichlet_boundary('West')
-            
+
             dc_east = np.append(bound_2, U_2[hr+1:,-1])
             dc_east = omega*dc_east + (1-omega)*Omega_2.get_Dirichlet_boundary('East')
             
@@ -92,7 +88,7 @@ for i in range(n):
         bound_N_2 = bound_N_2[0:Omega_1.N]
         comm.send(bound_N_1, dest = 1, tag = 2)
         comm.send(bound_N_2, dest = 3, tag = 3)
-        if i == n-1:
+        if i == 9:
             comm.send(U_2, dest = 0, tag = 12)
             #print(f'U1 = \n {U_1}')
     if rank == 1: #Omega 1
@@ -102,19 +98,23 @@ for i in range(n):
         U_1 = Omega_1.get_solutions()
         bound_1 = Omega_1.get_Dirichlet_boundary('East')
         comm.send(bound_1, dest = 2, tag = 1)
-        if i == n-1:
+        if i == 9:
             comm.send(U_1, dest = 0, tag = 11)
         
     if rank == 3: # Omega 3
         bound_N_2 = comm.recv(source = 2, tag = 3)
+        
         bound_N_2 = omega*bound_N_2 + (1-omega)*Omega_3.get_Neumann_boundary('West')
+        # bound_N_2[0] = 15
+        # print(bound_N_2)
         Omega_3.set_Neumann_boundary('West', bound_N_2)
         U_3_past = U_3
+
         U_3 = Omega_3.get_solutions()
         #U_3 = omega*U_3 + (1-omega)*U_3_past # Relaxation
         bound_2 = Omega_3.get_Dirichlet_boundary('West')
         comm.send(bound_2, dest = 2, tag = 4)
-        if i == n-1:
+        if i == 9:
             comm.send(U_3, dest = 0, tag = 10)
     
     if rank == 0:
@@ -127,6 +127,7 @@ for i in range(n):
         print(f'U_2final = \n {U_2_final}')
         print(f'U_3final = \n {U_3_final}')
         """
+        
         plot_and_save_heatmap(U_1_final, 'U_1_final')
         plot_and_save_heatmap(U_2_final, 'U_2_final')
         plot_and_save_heatmap(U_3_final, 'U_3_final')
@@ -139,7 +140,7 @@ for i in range(n):
         NW_N = N_square - 1
         
         NW = np.zeros((NW_N, NW_N))
-        
+
         # Remove last column to be able to block correctly
         SW = U_1_final[0:, 0:-1]
         
@@ -148,20 +149,20 @@ for i in range(n):
     
         West = np.block( [ [NW],[SW]  ]  )
         
-        West_Middle = np.block( [West, U_2_final[0:,0:-1] ])
-        print(U_2_final.shape[0])
+        West_Middle = np.block( [West, U_2_final])
         
-        NE = U_3_final[0:, 0:]
+        
+        NE = U_3_final[0:, 1:]
         SE = np.zeros((N_square, N_square))
-        SE = SE[0:-1, 0:]
+        SE = SE[0:-1, 1:]
         SE.fill(None)
 
         East = np.block([[NE], [SE]])   
         Apartment = np.block([West_Middle, East])
         
         plot_and_save_heatmap(Apartment, 'Apartment Projekt 3 pls')
-        
-        
+        sys.exit()
+
 
 
        
